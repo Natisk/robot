e1e5394c9a196ff1273c3e08df7cb94a061ea6a7
Vitaly Matseiko <realnatisk@gmail.com>
2025-10-24 09:39:32 +0200

    use single quotes

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-require_relative "toy_robot/version"
-require_relative "toy_robot/game"
-require_relative "toy_robot/platform"
-require_relative "toy_robot/robot"
+require_relative 'toy_robot/version'
+require_relative 'toy_robot/game'
+require_relative 'toy_robot/platform'
+require_relative 'toy_robot/robot'
 
+# Main namespace for the ToyRobot gem.
+# Provides an entry point to start the interactive robot simulation game.
 module ToyRobot
   def self.start
     Game.new.start
